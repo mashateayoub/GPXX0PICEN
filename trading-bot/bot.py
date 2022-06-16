eb4c117a2bca7a3854e3df98a766839a208bed51
Mashate Ayoub <68016682+mashateayoub@users.noreply.github.com>
2022-06-16 15:10:08 +0100

    making bot

--- trading-bot/bot.py
+++ trading-bot/bot.py
@@ -61,3 +61,38 @@ def get_positions():
         'APCA-API-SECRET-KEY': SECRET_KEY,
     }
     return requests.request("GET", url, headers=headers).json()
+
+
+def get_moving_averages(ticker):
+    """
+    calculates the 9 day and 30 days moving average of the specified ticker
+    :param ticker: stock ticker
+    :return: returns (9 days moving average, 30 days moving average)
+    """
+    data = yf.download(ticker, period="3mo", interval='1d')  # Download the last 3months worht of data for the ticker
+    data['SMA_9'] = data['Close'].rolling(window=9, min_periods=1).mean()   # Compute a 9-day Simple Moving Average with pandas
+    data['SMA_30'] = data['Close'].rolling(window=30, min_periods=1).mean()  # Compute a 30-day Simple Moving Average with pandas
+    SMA_9 = float(data.tail(1)["SMA_9"])  # Get the latest calculated 9 days Simple Moving Average
+    SMA_30 = float(data.tail(1)["SMA_30"]) # Get the latest 30 days Simple Moving Average
+    return SMA_9, SMA_30
+
+
+if __name__ == "__main__":
+    print("Starting the trading algorithm")
+    while True:
+        if pycron.is_now('30 9-15 * * 1-5', dt=datetime.now(timezone('EST'))):
+            ticker = "SPY"
+            SMA_9, SMA_30 = get_moving_averages(ticker)
+            if SMA_9 > SMA_30:
+                # We should buy if we don't already own the stock
+                if ticker not in [i["symbol"] for i in get_positions()]:
+                    print("Currently buying", ticker)
+                    buy_operation(ticker, 1)
+            if SMA_9 < SMA_30:
+                # We should liquidate our position if we own the stock
+                if ticker in [i["symbol"] for i in get_positions()]:
+                    print("Currently liquidating our", ticker, "position")
+                    close_position(ticker)
+            time.sleep(60) # Making sure we don't run the logic twice in a minute
+        else:
+            time.sleep(20)  # Check again in 20 seconds
